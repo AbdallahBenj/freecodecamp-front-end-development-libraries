705be3b6738369bef9040e1bde0a8328f4e131c8
AbdallahBenj <abdallahbenjalal@gmail.com>
2025-12-23 13:24:32 +0100

    fix base path for 01-app

--- exercises/01-react-basics/vite.config.js
+++ exercises/01-react-basics/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/freecodecamp-front-end-development-libraries/tree/main/exercises/01-react-basics/",
+  base: "/freecodecamp-front-end-development-libraries/01-react-basics/",
   plugins: [react()],
 });
